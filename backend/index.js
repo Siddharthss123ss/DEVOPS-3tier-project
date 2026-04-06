3612bfbb65bb32f010cbfffe8d69a7697be81e1d
Siddharthss123ss <siddharth.ss169ss@gmail.com>
2026-04-06 18:14:28 +0530

    ip updatess

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,18 @@
 const express = require("express");
-const db = require("./db");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend running v2 🚀");
+  res.send("Backend running on EC2 🚀");
 });
 
 app.get("/users", (req, res) => {
-  db.query("SELECT * FROM users", (err, result) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.json(result);
-  });
+  res.json([
+    { id: 1, name: "Siddharth" },
+    { id: 2, name: "Rahul" }
+  ]);
 });
 
-setTimeout(() => {
-  app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running");
-  });
-}, 8000);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running");
+});
